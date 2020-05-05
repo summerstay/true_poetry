74f77fb529805bc1b18ab6affb2cf53d64849a1f
summerstay <jsummerstay@gmail.com>
2020-05-05 11:04:27 -0400

    Add files via upload

--- true_poetry.py
+++ true_poetry.py
@@ -2,17 +2,13 @@ import string as str
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch.nn.functional as F
-#import pickle  
 from random import randint
-#import os
 import copy
-#import win_unicode_console 
-#import re
-#import copy
 import math
 
 # text to meter
 def text_to_meter(text, stress_dictionary):
+    #calculates the meter of a line of text.
     if len(text)==0:
         return ''
     #capitalize the text
@@ -32,7 +28,9 @@ def text_to_meter(text, stress_dictionary):
     return line_stress
 
 def rhyme_check(text1,text2,rhyme_dictionary):
+    #checks whether text1 and text2 rhyme according to a pronunciation dictionary.
     if text2 =="":
+        #everything rhymes with the empty text. Needed for starting condition.
         return True
     else:
         text1_words = text1.split(" ")
@@ -40,6 +38,7 @@ def rhyme_check(text1,text2,rhyme_dictionary):
         text2_words = text2.split(" ")
         last_word2 = text2_words[-1]
         if last_word1 == last_word2:
+            #prevent a word rhyming with itself
             return False
         elif last_word1 in rhyme_dictionary and last_word2 in rhyme_dictionary:
             if rhyme_dictionary[last_word1] == rhyme_dictionary[last_word2]:
@@ -49,51 +48,23 @@ def rhyme_check(text1,text2,rhyme_dictionary):
         else:
             return False
 
-pronounce_file = open("pronounce.txt", "r")
-stress_dictionary = {}
-for line in pronounce_file:
-    line = line.strip("\n")
-    parts = line.split(" ")
-    syllable_list = parts[2:]
-    word = parts[0]
-    stresses=""
-    if word in ["A","AN","THE","AND","BUT","OR"]:
-        stresses="~"
-    elif word in ["I","YOU","HE","SHE","IT","WE","THEY","MY","HIS","HER","ITS","OUR","YOUR","THEIR","OURS","YOURS","THEIRS","AM","IS","ARE","WAS","WERE","BEEN","BE","HAVE","HAS","HAD","DO","DOES","DID","WILL","WOULD","SHALL","SHOULD","MAY","MIGHT","MUST","CAN","COULD","OF","WITH","AT","FROM","TO","IN","FOR","ON","BY","LIKE","SINCE","UP","OFF","NEAR","WHICH","AS","EACH","SO","THAT","THATS"]:
-        stresses="?"    
-    else:
-        for syllable in syllable_list:
-            if syllable.endswith("1"):
-                stresses=stresses+"`"
-            elif syllable.endswith("0"):
-                stresses=stresses+"~"
-            elif syllable.endswith("2"):
-                stresses=stresses+"?"
-    stress_dictionary[word] = stresses
-
-pronounce_file = open("pronounce.txt", "r")
-rhyme_dictionary = {}
-reverse_rhyme_dictionary = {}
-for line in pronounce_file:
-    line = line.strip()
-    if line.startswith(';'): continue
-    word, phones = line.split("  ")
-    syllables = phones.split(" ")
-    join_flag = 0
-    outstring = ''
-    for syllable in syllables:
-        if join_flag == 0:
-            if "1" in syllable:
-                join_flag = 1
-                outstring = syllable
-        else:
-            outstring = outstring + " " + syllable
-    rhyme_dictionary[word.lower()] = outstring
-    if outstring in reverse_rhyme_dictionary:
-        reverse_rhyme_dictionary[outstring].append(word.lower())
-    else:
-        reverse_rhyme_dictionary[outstring]=[word.lower()]
-
+def compare_meters(meter1,meter2):
+    #checks whether meter1 is plausibly matching meter2. meter1 can include unknown ? stresses. 
+    matchflag=False
+    if len(meter1)<=len(meter2):
+        matchflag=True
+        for character1,character2 in zip(meter1,meter2):
+            if (character1=="`" and character2=="`") or (character1=="~" and character2=="~") or character1=="?":
+                pass
+            else:
+                matchflag=False
+    if len(meter1)==0:
+        matchflag = True  
+    #If you want to force it to end on a strongly stressed word, uncomment this.
+    #elif meter1[-1] == '?':
+    #    matchflag = False  
+    return matchflag
+       
 def grow_branches(sentence_so_far, probs, input_probability,past, h, prompt_length,rhyme,target_meter):
     #recursive function to find all sentence completions
     global complete_list
@@ -107,6 +78,7 @@ def grow_branches(sentence_so_far, probs, input_probability,past, h, prompt_leng
     text_sentence = tokenizer.decode(sentence_so_far[prompt_length:])
     meter = text_to_meter(text_sentence,stress_dictionary)
     offset = randint(0,3)
+    #we only want to search through "good" terms. Since the last term has to rhyme, we loosen the definition of good a little.
     if len(meter)>len(target_meter)-2:
         short_probability_list = sorted_probability_list[0+offset:200+offset]
     else:
@@ -117,6 +89,7 @@ def grow_branches(sentence_so_far, probs, input_probability,past, h, prompt_leng
         out_sentence = copy.deepcopy(sentence_so_far)
         sentence_and_probability = (out_sentence, input_probability)
         meter_check = compare_meters(meter,target_meter)
+        #this is all tokens that are just punctuation. Ugly, but it prevents endless strings of punctuation.
         foo = set(range(0, 94))
         punctuation = foo.union({438,492,526,553,650,764,834,837,986,1106,2109,2644,3228,3548,3557,4032,4480,4907,6329,7061,7874,9816,10185,11485,12248,12359,16317,17569,22135,22857,25780,34507,43179,44807,44825})
         #print(len(meter), end =" ")
@@ -130,12 +103,9 @@ def grow_branches(sentence_so_far, probs, input_probability,past, h, prompt_leng
                     print(text_sentence)
                     return 1
                 print(text_sentence, end = "\t")
-                #print(sentence_so_far, end = "\t")
                 return 2
-                #print(sentence_so_far)  
             else:
                 print(text_sentence, end = "\t")
-                #print(sentence_so_far, end = "\t")
                 return 2
         if next_probability < h or (not meter_check) or (sentence_so_far[-1] in punctuation and this_token in punctuation) or (len(sentence_so_far[prompt_length+1:])>30):
             pass
@@ -161,26 +131,60 @@ def expand_node(sentence, past):
         logits = logits[:, -1, :]  
         probs = F.softmax(logits, dim=-1).tolist()[0]
         return (probs, past)
-    
-def compare_meters(meter1,meter2):
-    matchflag=False
-    if len(meter1)<=len(meter2):
-        matchflag=True
-        for character1,character2 in zip(meter1,meter2):
-            if (character1=="`" and character2=="`") or (character1=="~" and character2=="~") or character1=="?":
-                pass
-            else:
-                matchflag=False
-    if len(meter1)==0:
-        matchflag = True   
-    #elif meter1[-1] == '?':
-    #    matchflag = False  
-    return matchflag
-                
+
+#create the stress dictionary    
+pronounce_file = open("pronounce.txt", "r")
+stress_dictionary = {}
+for line in pronounce_file:
+    line = line.strip("\n")
+    parts = line.split(" ")
+    syllable_list = parts[2:]
+    word = parts[0]
+    stresses=""
+    if word in ["A","AN","THE","AND","BUT","OR"]:
+        stresses="~"
+    elif word in ["I","YOU","HE","SHE","IT","WE","THEY","MY","HIS","HER","ITS","OUR","YOUR","THEIR","OURS","YOURS","THEIRS","AM","IS","ARE","WAS","WERE","BEEN","BE","HAVE","HAS","HAD","DO","DOES","DID","WILL","WOULD","SHALL","SHOULD","MAY","MIGHT","MUST","CAN","COULD","OF","WITH","AT","FROM","TO","IN","FOR","ON","BY","LIKE","SINCE","UP","OFF","NEAR","WHICH","AS","EACH","SO","THAT","THATS"]:
+        stresses="?"    
+    else:
+        for syllable in syllable_list:
+            if syllable.endswith("1"):
+                stresses=stresses+"`"
+            elif syllable.endswith("0"):
+                stresses=stresses+"~"
+            elif syllable.endswith("2"):
+                stresses=stresses+"?"
+    stress_dictionary[word] = stresses
+
+#create the rhyme dictionary
+pronounce_file = open("pronounce.txt", "r")
+rhyme_dictionary = {}
+reverse_rhyme_dictionary = {}
+for line in pronounce_file:
+    line = line.strip()
+    if line.startswith(';'): continue
+    word, phones = line.split("  ")
+    syllables = phones.split(" ")
+    join_flag = 0
+    outstring = ''
+    for syllable in syllables:
+        if join_flag == 0:
+            if "1" in syllable:
+                join_flag = 1
+                outstring = syllable
+        else:
+            outstring = outstring + " " + syllable
+    rhyme_dictionary[word.lower()] = outstring
+    if outstring in reverse_rhyme_dictionary:
+        reverse_rhyme_dictionary[outstring].append(word.lower())
+    else:
+        reverse_rhyme_dictionary[outstring]=[word.lower()]
+
+#load gpt-2 (takes a few seconds)                
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')   
 model = GPT2LMHeadModel.from_pretrained("poetry")
 print("model loaded")
 
+#from here on must be run every time you want to create a new poem.
 leaf_list = []
 branch_list = []
 complete_list = []
@@ -191,13 +195,17 @@ with torch.no_grad():
     original_length = len(prompt)
     past = None
     (probs, past) = expand_node(prompt, None) 
+    #lines that don't need to rhyme are given empty strings
     rhyme = ""
     target_meter ="~`~`~`~`~`"
     grow_branches(prompt,probs,1,past,probability_threshhold,len(prompt),rhyme,target_meter)
+    #the new line to rhyme with is the just created line
     rhyme = tokenizer.decode(complete_list[-1][0])
+    #the new prompt is the previous prompt plus the new line, with newlines.
     prompt = prompt + [198] + complete_list[-1][0] +[198]
     (probs, past) = expand_node(prompt, None) 
     grow_branches(prompt,probs,1,past,probability_threshhold,len(prompt),rhyme,target_meter)
+    #this next line doesn't rhyme with anything.
     rhyme ="" 
     prompt = prompt + [198] + complete_list[-1][0] +[198]
     (probs, past) = expand_node(prompt, None) 
